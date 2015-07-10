85c851699a0081c48cc05131fd82c5052603a42a
zbfunplus <zhangbiao2000@yeah.net>
2015-07-10 16:13:34 +0800

    string class

---
+++ string.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+struct StringRep{
+	StringRep(const char* str)
+	{
+		cout<<"create"<<endl;
+		p = strdup(str);
+		refCount = 1;
+	}
+	~StringRep(){
+		delete p;
+	}
+	char* p;
+	int refCount;
+};
+
+class String{
+	public:
+		String():sp(NULL) {}
+		String(const char* str)
+			:sp(new StringRep(str)) {}
+		String(const String& s)
+		{
+			sp = s.sp;
+			sp->refCount++;
+		}
+		~String(){
+			cout<<"destruct, refCount="<<sp->refCount<<endl;
+			if(--sp->refCount == 0){
+				cout<<"call delete"<<endl;
+				delete sp;
+			}
+		}
+		String operator=(String s)	// exception safe assignment
+		{
+			cout<<"call ="<<endl;
+			swap(sp, s.sp);
+			return *this;
+		}
+		int refCount(){
+			return sp->refCount;
+		}
+
+	private:
+		StringRep* sp;
+};
+
+int main()
+{
+	String s("hello");
+	cout<<s.refCount()<<endl;
+	String s2;
+	s2 = s;
+	cout<<s.refCount()<<endl;
+	return 0;
+}
