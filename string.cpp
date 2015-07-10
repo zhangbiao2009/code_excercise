2aeaf05780e0b3c2a2714314ab72da13170a3a8c
zbfunplus <zhangbiao2000@yeah.net>
2015-07-10 08:31:35 +0000

    check sp is null in destructor and return String& for assignment

--- string.cpp
+++ string.cpp
@@ -4,56 +4,56 @@
 using namespace std;
 
 struct StringRep{
-	StringRep(const char* str)
-	{
-		cout<<"create"<<endl;
-		p = strdup(str);
-		refCount = 1;
-	}
-	~StringRep(){
-		delete p;
-	}
-	char* p;
-	int refCount;
+    StringRep(const char* str)
+    {
+        cout<<"create"<<endl;
+        p = strdup(str);
+        refCount = 1;
+    }
+    ~StringRep(){
+        delete p;
+    }
+    char* p;
+    int refCount;
 };
 
 class String{
-	public:
-		String():sp(NULL) {}
-		String(const char* str)
-			:sp(new StringRep(str)) {}
-		String(const String& s)
-		{
-			sp = s.sp;
-			sp->refCount++;
-		}
-		~String(){
-			cout<<"destruct, refCount="<<sp->refCount<<endl;
-			if(--sp->refCount == 0){
-				cout<<"call delete"<<endl;
-				delete sp;
-			}
-		}
-		String operator=(String s)	// exception safe assignment
-		{
-			cout<<"call ="<<endl;
-			swap(sp, s.sp);
-			return *this;
-		}
-		int refCount(){
-			return sp->refCount;
-		}
+    public:
+        String():sp(NULL) {}
+        String(const char* str)
+            :sp(new StringRep(str)) {}
+        String(const String& s)
+        {
+            sp = s.sp;
+            sp->refCount++;
+        }
+        ~String(){
+            cout<<"destruct"<<endl;
+            if(sp && --sp->refCount == 0){
+                cout<<"call delete"<<endl;
+                delete sp;
+            }
+        }
+        String& operator=(String tmp)	// exception safe assignment
+        {
+            cout<<"call ="<<endl;
+            swap(sp, tmp.sp);
+            return *this;
+        }
+        int refCount(){
+            return sp->refCount;
+        }
 
-	private:
-		StringRep* sp;
+    private:
+        StringRep* sp;
 };
 
 int main()
 {
-	String s("hello");
-	cout<<s.refCount()<<endl;
-	String s2;
-	s2 = s;
-	cout<<s.refCount()<<endl;
-	return 0;
+    String s("hello");
+    cout<<s.refCount()<<endl;
+    String s2;
+    s2 = s;
+    cout<<s.refCount()<<endl;
+    return 0;
 }
