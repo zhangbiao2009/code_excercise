48fe3d3ed884bc95cdddbf86e26ce1e4798e8326
zbfunplus <zhangbiao2000@yeah.net>
2016-01-14 06:02:17 +0000

    ignore blank line and comment

--- line_counter/main.c
+++ line_counter/main.c
@@ -3,6 +3,9 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <stdbool.h>
+
+enum {INIT_STATE, COMMENT_STATE1, ONE_LINE_COMMENT_STATE, MULTI_LINE_COMMENT_BEGIN_STATE, MULTI_LINE_COMMENT_WAIT_END_STATE};
 
 int handle_file(const char* path)
 {
@@ -12,9 +15,80 @@ int handle_file(const char* path)
     int no_of_line = 0;
     
     int c;
+    bool need_count = false;
+    int state = INIT_STATE;
     while((c = fgetc(fp)) != EOF){
-        if(c == '\n')
-            no_of_line++;
+        switch(state){
+            case INIT_STATE:
+                if(c == '/')
+                    state = COMMENT_STATE1;
+                else if(!isspace(c))
+                    need_count = true;
+                else if(c == '\n'){
+                    if(need_count){
+                        no_of_line++;
+                        need_count = false;
+                    }
+                }
+                else{
+                    // do nothing
+                }
+                break;
+            case COMMENT_STATE1:
+                if(c == '/')
+                    state = ONE_LINE_COMMENT_STATE;
+                else if(c == '*')
+                    state = MULTI_LINE_COMMENT_BEGIN_STATE;
+                else {
+                    state = INIT_STATE;
+                    if(c == '\n'){
+                        no_of_line++;
+                        need_count = false;
+                    }
+                }
+                break;
+            case ONE_LINE_COMMENT_STATE:
+                if(c == '\n'){
+                    if(need_count){
+                        no_of_line++;
+                        need_count = false;
+                    }
+                    state = INIT_STATE;
+                }else{
+                    // do nothing
+                }
+                break;
+            case MULTI_LINE_COMMENT_BEGIN_STATE:
+                if(c == '*')
+                    state = MULTI_LINE_COMMENT_WAIT_END_STATE;
+                else if(c == '\n'){
+                    if(need_count){
+                        no_of_line++;
+                        need_count = false;
+                    }
+                    // note: no need to change state
+                }
+                else{
+                    // do nothing
+                }
+                break;
+            case MULTI_LINE_COMMENT_WAIT_END_STATE:
+                if(c == '/')    // comment end
+                    state = INIT_STATE;
+                else if(c == '\n'){
+                    if(need_count){
+                        no_of_line++;
+                        need_count = false;
+                    }
+                    state = MULTI_LINE_COMMENT_BEGIN_STATE;
+                }
+                else
+                    state = MULTI_LINE_COMMENT_BEGIN_STATE;
+                break;
+            default:
+                fprintf(stderr, "unkown state: %d\n", state);
+                break;
+        }
     }
     return no_of_line;
 }
