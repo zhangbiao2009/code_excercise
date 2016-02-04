bfd2ebe07505cce3a30040cf94f2f70af6bae478
zbfunplus <zhangbiao2000@yeah.net>
2016-02-04 02:39:08 +0000

    simplify code

--- traverse_array_like_clip.c
+++ traverse_array_like_clip.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+#define M 3
+#define N 3
+int a[M][N] = {
+    0x1, 0x2, 0x3,
+    0x8, 0x9, 0x4,
+    0x7, 0x6, 0x5,
+};
+
+
+/*
 #define M 4
 #define N 3
 int a[M][N] = {
@@ -8,11 +18,10 @@ int a[M][N] = {
     0x9, 0xc, 0x5,
     0x8, 0x7, 0x6,
 };
+*/
 
-int up_border = 0;
-int down_border = M-1;
-int left_border = 0;
-int right_border = N-1;
+int nx = N;
+int ny = M;
 
 struct pos{
     int x;
@@ -28,61 +37,49 @@ enum Direction curr_dir = Right;
 void print()
 {
     while(1){
-        // go to border
-        int finished = gotoBorder();
-        if(finished)
-            break;
+        switch(curr_dir){
+            case Right:
+                for(int i=0; i<nx; i++){
+                    printf("%x\n", a[curr_pos.y][curr_pos.x]);
+                    if(i < nx-1)        // 最后一次坐标不变
+                        curr_pos.x++;
+                }
+                ny--;           // 走完一行
+                curr_pos.y++;   // 挪到下一个开始点
+                break;
+            case Down:
+                for(int i=0; i<ny; i++){
+                    printf("%x\n", a[curr_pos.y][curr_pos.x]);
+                    if(i < ny-1)        // 最后一次坐标不变
+                        curr_pos.y++;
+                }
+                nx--;           // 走完一列
+                curr_pos.x--;   // 挪到下一个开始点
+                break;
+            case Left:
+                for(int i=0; i<nx; i++){
+                    printf("%x\n", a[curr_pos.y][curr_pos.x]);
+                    if(i < nx-1)        // 最后一次坐标不变
+                        curr_pos.x--;
+                }
+                ny--;           // 走完一行
+                curr_pos.y--;   // 挪到下一个开始点
+                break;
+            case Up:
+                for(int i=0; i<ny; i++){
+                    printf("%x\n", a[curr_pos.y][curr_pos.x]);
+                    if(i < ny-1)        // 最后一次坐标不变
+                        curr_pos.y--;
+                }
+                nx--;           // 走完一列
+                curr_pos.x++;   // 挪到下一个开始点
+                break;
+        }
+        if(nx<=0 || ny<=0)
+            return;
         // change direction
         curr_dir = (curr_dir+1)%nDirection;
     }
-}
-
-int gotoBorder()
-{
-    while(1){
-        printf("%x\n", a[curr_pos.y][curr_pos.x]);
-        //print
-        switch(curr_dir){
-            case Right:
-                if(curr_pos.x == right_border){
-                    up_border++;        // 走完一行
-                    curr_pos.y++;       // 挪到下一个开始点
-                    goto exit;
-                }
-                curr_pos.x++;
-                break;
-            case Down:
-                if(curr_pos.y == down_border){
-                    right_border--;     // 走完一列
-                    curr_pos.x--;
-                    goto exit;
-                }
-                curr_pos.y++;
-                break;
-            case Left:
-                if(curr_pos.x == left_border){
-                    down_border--;        // 走完一行
-                    curr_pos.y--;
-                    goto exit;
-                }
-                curr_pos.x--;
-                break;
-            case Up:
-                if(curr_pos.y == up_border){
-                    left_border++;     // 走完一列
-                    curr_pos.x++;
-                    goto exit;
-                }
-                curr_pos.y--;
-                break;
-        }
-    }
-
-exit:
-    if(up_border > down_border || left_border > right_border)
-        return 1;
-    else
-        return 0;
 }
 
 int main()
