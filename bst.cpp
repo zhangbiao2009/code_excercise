686a39e7d9ef7dcc9993986f61109d3cba633a59
zbfunplus <zhangbiao2000@yeah.net>
2016-03-04 09:28:41 +0000

    non-recursive traverse

---
+++ bst.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+struct Node{
+    bool accessed;
+    int data;
+    Node* left;
+    Node* right;
+    Node(int v, Node* l=NULL, Node* r=NULL)
+        : accessed(false), data(v), left(l), right(r) {}
+};
+
+class BST{
+    private:
+        Node* root;
+        void insert(Node** np, int v)
+        {
+            if(*np == NULL){
+                *np = new Node(v);
+                return;
+            }
+            if(v < (*np)->data)
+                insert(&(*np)->left, v);
+            else
+                insert(&(*np)->right, v);
+        }
+
+        void traverse(Node* np)
+        {
+            if(np == NULL)
+                return;
+            traverse(np->left);
+            cout<<np->data<<endl;
+            traverse(np->right);
+        }
+
+        void traverse2(Node* np)        // 非递归，这种方式的缺点是需要一个accessed标记
+        {
+            stack<Node*> s;
+            s.push(np);
+            while(!s.empty()){
+                Node* tmp = s.top();
+                s.pop();
+                if(tmp){
+                    if(!tmp->accessed){
+                        s.push(tmp->right);
+                        tmp->accessed = true;
+                        s.push(tmp);
+                        s.push(tmp->left);
+                    }
+                    else{
+                        cout<<tmp->data<<endl;
+                    }
+                }
+            }
+        }
+
+        void traverse3(Node* np)        // 非递归，无需标记
+        {
+            stack<Node*> s;
+
+            Node* needp = np;
+            while(1){
+                for(auto p = needp; p != NULL; p = p->left)
+                    s.push(p);
+
+                if(!s.empty()){
+                    Node* tmp = s.top();
+                    s.pop();
+                    cout<<tmp->data<<endl;
+                    needp = tmp->right;
+                }
+                else break;
+            }
+        }
+
+    public:
+        BST(const vector<int> nodes)
+            :root(NULL)
+        {
+            for(int i=0; i<nodes.size(); i++)
+                insert(&root, nodes[i]);
+        }
+        void traverse()
+        {
+            traverse3(root);
+        }
+
+};
+
+
+int main()
+{
+    BST tree(vector<int>{5,3,9,6,10,4,2,7});
+    tree.traverse();
+    return 0;
+}
